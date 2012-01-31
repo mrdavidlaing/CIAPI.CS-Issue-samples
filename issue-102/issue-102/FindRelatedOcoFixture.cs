7d4b51f7eb3dedd668f14944aa57092761909f49
David Laing <david@davidlaing.com>
2012-01-31 12:56:10 +0000

    Include sample of JSON recieved from API

--- issue-102/issue-102/FindRelatedOcoFixture.cs
+++ issue-102/issue-102/FindRelatedOcoFixture.cs
@@ -28,8 +28,19 @@ namespace issue_102
             
             // 3) Grab the related stop and limit orders fot this open position
             var stopOrder = client.TradesAndOrders.GetActiveStopLimitOrder(openpositions.OpenPositions[0].StopOrder.OrderId.ToString());
+            /*
+                Request URI     : https://ciapi.cityindex.com/tradingapi/order/473037563/activestoplimitorder
+                ResponseText    : {"ActiveStopLimitOrder":{"Applicability":2,"Currency":"USD","Direction":"sell","ExpiryDateTimeUTC":null,"LastChangedDateTimeUTC":"\/Date(1327946960482)\/","LimitOrder":null,"MarketId":400481134,"MarketName":"EUR\/USD",
+             *                                              "OcoOrder":{"OrderId":473037507,"Quantity":5000,"TriggerPrice":1.5},
+             *                                              "OrderId":473037563,"ParentOrderId":473037506,"Quantity":5000,"Status":2,"StopOrder":null,"TradingAccountId":400258216,"TriggerPrice":1.2,"Type":2}}
+            */
             var limitOrder = client.TradesAndOrders.GetActiveStopLimitOrder(openpositions.OpenPositions[0].LimitOrder.OrderId.ToString());
-
+            /*
+                Request URI     : https://ciapi.cityindex.com/tradingapi/order/473037507/activestoplimitorder
+                ResponseText    : {"ActiveStopLimitOrder":{"Applicability":2,"Currency":"USD","Direction":"sell","ExpiryDateTimeUTC":null,"LastChangedDateTimeUTC":"\/Date(1327946876905)\/","LimitOrder":null,"MarketId":400481134,"MarketName":"EUR\/USD",
+             *                                              "OcoOrder":null,
+             *                                              "OrderId":473037507,"ParentOrderId":473037506,"Quantity":5000,"Status":2,"StopOrder":null,"TradingAccountId":400258216,"TriggerPrice":1.5,"Type":3}}
+            */
 
             // 4) Now, check that stopOrder is related to the limitOrder via the OcoOrder
             Assert.IsNotNull(stopOrder.ActiveStopLimitOrder.OcoOrder, "stopOrder has no OcoOrder");
