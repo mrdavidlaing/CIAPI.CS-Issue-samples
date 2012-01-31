3c52cc6f9dde364359d4effa750299ed6d536ccd
David Laing <david@davidlaing.com>
2012-01-31 12:51:46 +0000

    Add comments & JsConsole equivalent

--- issue-102/issue-102/FindRelatedOcoFixture.cs
+++ issue-102/issue-102/FindRelatedOcoFixture.cs
@@ -16,22 +16,53 @@ namespace issue_102
         [Test]
         public void FindRelatedOco()
         {
+            // 1) Logon to the Flex ITP and configure your account (a CFD only account) so that you have only one open position with an attached stop and limit order
+            // https://ciapi.cityindex.com/flexitp
+
+            // 2) Grab the open positions for this account
             var client = new CIAPI.Rpc.Client(new Uri(serviceUrl));
             client.LogIn(username, password);
-
             var accountInfo = client.AccountInformation.GetClientAndTradingAccount();
+
             var openpositions = client.TradesAndOrders.ListOpenPositions(accountInfo.TradingAccounts[0].TradingAccountId);
+            
+            // 3) Grab the related stop and limit orders fot this open position
             var stopOrder = client.TradesAndOrders.GetActiveStopLimitOrder(openpositions.OpenPositions[0].StopOrder.OrderId.ToString());
             var limitOrder = client.TradesAndOrders.GetActiveStopLimitOrder(openpositions.OpenPositions[0].LimitOrder.OrderId.ToString());
 
-            //We assume that the stopOrder will be related to the limitOrder via OcoOrder
-            Assert.IsNotNull(stopOrder.ActiveStopLimitOrder.OcoOrder);
-            Assert.AreEqual(stopOrder.ActiveStopLimitOrder.OcoOrder.OrderId, limitOrder.ActiveStopLimitOrder.OrderId);
 
-            //And that the limitOrder will be related to the stopOrder via OcoOrder
-            Assert.IsNotNull(limitOrder.ActiveStopLimitOrder.OcoOrder);
-            Assert.AreEqual(limitOrder.ActiveStopLimitOrder.OcoOrder.OrderId, stopOrder.ActiveStopLimitOrder.OrderId);
+            // 4) Now, check that stopOrder is related to the limitOrder via the OcoOrder
+            Assert.IsNotNull(stopOrder.ActiveStopLimitOrder.OcoOrder, "stopOrder has no OcoOrder");
+            Assert.AreEqual(stopOrder.ActiveStopLimitOrder.OcoOrder.OrderId, limitOrder.ActiveStopLimitOrder.OrderId, "stopOrder's Oco is not the limitOrder as expected");
 
+            // 5) Now, check that reverse is also true - ie., the limitOrder is related to the stopOrder via the OcoOrder
+            Assert.IsNotNull(limitOrder.ActiveStopLimitOrder.OcoOrder, "limitOrder has no OcoOrder");
+            Assert.AreEqual(limitOrder.ActiveStopLimitOrder.OcoOrder.OrderId, stopOrder.ActiveStopLimitOrder.OrderId, "limitOrder's Oco is not the stopOrder as expected");
+
+            /**
+             * Replicate this behaviour using the JS test console at https://ciapi.cityindex.com/tradingapi
+             *  
+                    var userName = "xx747649";
+                    doPost('/session',{ "UserName": userName, "Password": "password"}, function (data, textCode) {
+                       setRequestHeader("UserName", userName);
+                       setRequestHeader("Session", data.Session);
+
+                       doGet('/UserAccount/ClientAndTradingAccount');  //TradingAccountId is 400258216
+                    //grab all open positions
+                       doGet('/order/openpositions?TradingAccountId=400258216');
+                    //Which gives us position 473037506, that has a stop (473037563) & limit order (473037507)
+
+                    // Get all orders - there should be 2. 
+                       doGet('/order/activestoplimitorders?tradingaccountid=400258216');
+
+                    // grab the stop - note that the attached OcoOrder is the limit
+                       doGet('/order/473037563/activestoplimitorder');
+
+                    //grab the limit - note that the limit has no attached OcoOrder
+                       doGet('/order/473037507/activestoplimitorder');
+
+                    }); 
+             */
         }
     }
 }
